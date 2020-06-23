c7e7fba5b56adba98106be465c4eb650952a34ca
baselinealpha <baselinealphax@gmail.com>
2020-06-23 04:37:56 +0100

    The game works!

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# rock-paper-scissors
+# rock-paper-scissors  
+
+A game I made as part of a personal challenge (inspired by the 100 days of code) to code everyday.  
+It was made with the basic knowledge of javascript I currently possess and though it took me shorter than I expected, I know the code definitely is very bulky.  
+
+As I code on and learn simpler ways to do things, I might simplify the code behind this one (if I have the drive to).  
+If anyone is reading this, contributions and suggestions also make me very glad.  
---
+++ game.js
@@ -0,0 +1,123 @@
+function rulesToggle(){
+    let display = document.getElementById("rules-div").style.display
+    if (display == "block") {
+        display = "none"
+    }
+    else {
+        display = "block"
+    }
+    document.getElementById("rules-div").style.display = display;
+}
+
+function rulesClose(){
+    document.getElementById("rules-div").style.display = "none";
+}
+
+
+
+
+
+function rockFunction(){
+    document.getElementById("choose-text").style.display = "none"
+    document.getElementById("game-results-div").style.display = "flex";
+    document.getElementById("rock-button").disabled = true;
+    document.getElementById("paper-button").disabled = true;
+    document.getElementById("scissors-button").disabled = true;
+    let userOption = "Rock";
+    let statusDisplay = `You chose ${userOption}`;
+    document.getElementById("game-status-display").innerHTML = statusDisplay;
+    document.getElementById("game-div").style.animation = "game-div-spin 1s 1 linear";
+    let aiPick = Math.random();
+    if (aiPick < 0.67) {
+        if (aiPick < 0.34) {
+            aiOption = "Rock";
+            document.getElementById("game-status-display").innerHTML = `${statusDisplay} and I chose ${aiOption}`;
+            document.getElementById("game-output-display").innerHTML = "Draw! Play again"
+        }
+        else {
+            aiOption = "Paper";
+            document.getElementById("game-status-display").innerHTML = `${statusDisplay} and I chose ${aiOption}`;
+            document.getElementById("game-output-display").innerHTML = "You've been wrapped. I win!"
+        }
+    }
+    else {
+        aiOption = "Scissors";
+        document.getElementById("game-status-display").innerHTML = `${statusDisplay} and I chose ${aiOption}`;
+        document.getElementById("game-output-display").innerHTML = "Oh shit :-( You win!"
+    }
+}
+
+
+
+function scissorsFunction(){
+    document.getElementById("choose-text").style.display = "none"
+    document.getElementById("game-results-div").style.display = "flex";
+    document.getElementById("rock-button").disabled = true;
+    document.getElementById("paper-button").disabled = true;
+    document.getElementById("scissors-button").disabled = true;
+    let userOption = "Scissors";
+    let statusDisplay = `You chose ${userOption}`;
+    document.getElementById("game-status-display").innerHTML = statusDisplay;
+    document.getElementById("game-div").style.animation = "game-div-spin 1s 1 linear";
+    let aiPick = Math.random();
+    if (aiPick < 0.67) {
+        if (aiPick < 0.34) {
+            aiOption = "Rock";
+            document.getElementById("game-status-display").innerHTML = `${statusDisplay} and I chose ${aiOption}`;
+            document.getElementById("game-output-display").innerHTML = "Well look who's broken. I win!"
+        }
+        else {
+            aiOption = "Paper";
+            document.getElementById("game-status-display").innerHTML = `${statusDisplay} and I chose ${aiOption}`;
+            document.getElementById("game-output-display").innerHTML = "I'm torn :-( You win!"
+        }
+    }
+    else {
+        aiOption = "Scissors";
+        document.getElementById("game-status-display").innerHTML = `${statusDisplay} and I chose ${aiOption}`;
+        document.getElementById("game-output-display").innerHTML = "Draw! Play again"
+    }
+}
+
+
+
+function paperFunction(){
+    document.getElementById("choose-text").style.display = "none"
+    document.getElementById("game-results-div").style.display = "flex";
+    document.getElementById("rock-button").disabled = true;
+    document.getElementById("paper-button").disabled = true;
+    document.getElementById("scissors-button").disabled = true;
+    let userOption = "Paper";
+    let statusDisplay = `You chose ${userOption}`;
+    document.getElementById("game-status-display").innerHTML = statusDisplay;
+    document.getElementById("game-div").style.animation = "game-div-spin 1s 1 linear";
+    let aiPick = Math.random();
+    if (aiPick < 0.67) {
+        if (aiPick < 0.34) {
+            aiOption = "Rock";
+            document.getElementById("game-status-display").innerHTML = `${statusDisplay} and I chose ${aiOption}`;
+            document.getElementById("game-output-display").innerHTML = "It's a wrap. You win!"
+        }
+        else {
+            aiOption = "Paper";
+            document.getElementById("game-status-display").innerHTML = `${statusDisplay} and I chose ${aiOption}`;
+            document.getElementById("game-output-display").innerHTML = "Draw! Play again"
+        }
+    }
+    else {
+        aiOption = "Scissors";
+        document.getElementById("game-status-display").innerHTML = `${statusDisplay} and I chose ${aiOption}`;
+        document.getElementById("game-output-display").innerHTML = "I win! Don't worry, you'll be fine..."
+    }
+}
+
+function resetFunction(){
+    document.getElementById("choose-text").style.display = "block"
+    document.getElementById("game-results-div").style.display = "none";
+    document.getElementById("rock-button").disabled = false;
+    document.getElementById("paper-button").disabled = false;
+    document.getElementById("scissors-button").disabled = false;
+    document.getElementById("game-status-display").innerHTML = "";
+    document.getElementById("game-output-display").innerHTML = ""
+    document.getElementById("game-div").style.animation = "";
+}
